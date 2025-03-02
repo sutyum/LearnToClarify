2380d199d4a2968c84fd28e1f9487790d3a85bbf
Satyam Tiwary <satyam@technoculture.io>
2025-03-03 00:28:30 +0530

    flash-attn still an issue in modal

--- modal_train.py
+++ modal_train.py
@@ -5,8 +5,7 @@ image = (
     modal.Image.from_registry("pytorch/pytorch:2.5.1-cuda12.1-cudnn9-devel")
     .apt_install("git")
     .pip_install("uv")
-    .pip_install("torch")
-    .pip_install("accelerate")
+    # .pip_install("accelerate")
 
     .workdir("/root")
     .add_local_file("pyproject.toml", "/root/LearnToClarify/pyproject.toml", copy=True)
@@ -14,8 +13,9 @@ image = (
     .add_local_file("train.py", "/root/LearnToClarify/train.py", copy=True)
     .add_local_file("gsm8k_simple.py", "/root/LearnToClarify/gsm8k_simple.py", copy=True)
 
-    .run_commands("cd /root/LearnToClarify && uv sync", force_build=True)
-    .run_commands("cd /root/LearnToClarify && uv pip install flash-attn --no-build-isolation")
+    .run_commands("cd /root/LearnToClarify && uv sync") #, force_build=True)
+    .run_commands("uv pip install torch")
+    .run_commands("uv pip install flash-attn --no-build-isolation")
 )
 
 # Define the app
